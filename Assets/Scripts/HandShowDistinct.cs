fda21d03d52f3f0b51e056d53fd654bd500f5c89
Ian-Huang <dk781020@hotmail.com>
2013-08-18 16:29:17 +0800

    Scene2 Update

--- Assets/Scripts/HandShowDistinct.cs
+++ Assets/Scripts/HandShowDistinct.cs
@@ -9,47 +9,60 @@ public class HandShowDistinct : MonoBehaviour
     public int Y_Min;
 
     public SkeletonInformation skeletonInformation;
-
-    private Vector3 OrginalLocalPosition;
     public int ratio; //影響倍率值
 
-
-    public enum HandType { Right, Left };
     public HandType handType;
 
-    RaycastHit hit;
-    public GameObject Target;
+    public LayerMask TargetLayer;
+
+    private RaycastHit hit;
+    private Vector3 OrginalLocalPosition;
+
     // Use this for initialization
     void Start()
     {
         this.OrginalLocalPosition = this.transform.localPosition;
-       
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (handType == HandType.Right)
         {
             this.transform.localPosition = this.OrginalLocalPosition + new Vector3(
-    Mathf.Clamp(KinectDetectOutput.RightHandPosition.x * ratio, X_Min, X_Max),
-    Mathf.Clamp(KinectDetectOutput.RightHandPosition.y * ratio, Y_Min, Y_Max),
-    0);
+                Mathf.Clamp(KinectDetectOutput.RightHandPosition.x * ratio, X_Min, X_Max),
+                Mathf.Clamp(KinectDetectOutput.RightHandPosition.y * ratio, Y_Min, Y_Max),
+                0);
         }
         else
         {
             this.transform.localPosition = this.OrginalLocalPosition + new Vector3(
-    Mathf.Clamp(KinectDetectOutput.LeftHandPosition.x * ratio, X_Min, X_Max),
-    Mathf.Clamp(KinectDetectOutput.LeftHandPosition.y * ratio, Y_Min, Y_Max),
-    0);
+                Mathf.Clamp(KinectDetectOutput.LeftHandPosition.x * ratio, X_Min, X_Max),
+                Mathf.Clamp(KinectDetectOutput.LeftHandPosition.y * ratio, Y_Min, Y_Max),
+                0);
         }
-
 
         //Raycast hit
-        if (Physics.Raycast(transform.position, new Vector3(0, 0, 1), out hit, 10) == true)
+        if (Physics.Raycast(transform.position, new Vector3(0, 0, 1), out hit, 10, this.TargetLayer) == true)
         {
-            Target = hit.transform.gameObject;
+            //if (KinectDetectOutput.isRocking)
+            //{
+            PictureColorController script = this.hit.collider.transform.parent.GetComponent<PictureColorController>();
+            script.DecPictureAlpha();
+            script.isStartRecove = false;
+            script.addValue = script.UndetectTime;
+            //}
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        //畫出偵測線
+        Gizmos.DrawRay(this.transform.position, new Vector3(0, 0, 1) * 10);
+    }
+
+    public enum HandType
+    {
+        Right = 0, Left = 1
     }
 }
--- Assets/Scripts/Scene2/PictureColorController.cs
+++ Assets/Scripts/Scene2/PictureColorController.cs
@@ -2,17 +2,138 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// last modify：2013-08-18 
+/// 圖片控制器
+/// </summary>
 public class PictureColorController : MonoBehaviour
 {
     public List<SmoothMoves.Sprite> SpriteList = new List<SmoothMoves.Sprite>();    //Sprite清單
-    public float ChangeScale;   //每次變動的幅度
+    public float ChangeDeltaScale;  //每次變動的幅度(0~1)
+    public float CountDownTime;     //每次變動的冷卻時間(秒)
 
-    private int currentIndex = 0;
+    private bool isChangeFinish;
+    private int currentIndex = 0;   //目前圖片的index
+    private float updateValue;
+
+    public float UndetectTime;
+    public float addValue;
+
+    public bool isStartRecove;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
+        this.isChangeFinish = true;
+        this.isStartRecove = false;
+        this.addValue = this.UndetectTime;
+    }
 
+    /// <summary>
+    /// ITween顏色改變每個Frame變化值
+    /// </summary>
+    /// <param name="value">變化值</param>
+    void ColorChangeUpdate(float value)
+    {
+        this.SpriteList[this.currentIndex].SetColor(new Color(1, 1, 1, value));
+    }
+
+    /// <summary>
+    /// ITween顏色改變完成後
+    /// </summary>
+    void ColorChangeComplete()
+    {
+        this.isChangeFinish = true;
+    }
+
+    /// <summary>
+    /// 減少圖片Alpha值
+    /// </summary>
+    public void DecPictureAlpha()
+    {
+        if (this.isChangeFinish)
+        {
+            if (this.SpriteList[this.currentIndex].color.a <= 0)
+            {
+                this.currentIndex++;
+                if (this.currentIndex >= this.SpriteList.Count - 1)
+                {
+                    this.currentIndex = this.SpriteList.Count - 2;
+                    this.SpriteList[this.currentIndex].color.a = 0;
+                }
+                else
+                {
+                    this.isChangeFinish = false;
+                    iTween.ValueTo(this.gameObject, iTween.Hash(
+                        "name", "ColorChange",
+                        "from", this.SpriteList[this.currentIndex].color.a,
+                        "to", this.SpriteList[this.currentIndex].color.a - this.ChangeDeltaScale,
+                        "time", this.CountDownTime,
+                        "onupdate", "ColorChangeUpdate",
+                        "onupdateparams", this.updateValue,
+                        "oncomplete", "ColorChangeComplete"
+                        ));
+                }
+            }
+            else
+            {
+                this.isChangeFinish = false;
+                iTween.ValueTo(this.gameObject, iTween.Hash(
+                    "name", "ColorChange",
+                    "from", this.SpriteList[this.currentIndex].color.a,
+                    "to", this.SpriteList[this.currentIndex].color.a - this.ChangeDeltaScale,
+                    "time", this.CountDownTime,
+                    "onupdate", "ColorChangeUpdate",
+                    "onupdateparams", this.updateValue,
+                    "oncomplete", "ColorChangeComplete"
+                    ));
+            }
+        }
+    }
+
+    /// <summary>
+    /// 增加圖片Alpha值
+    /// </summary>
+    public void AddPictureAlpha()
+    {
+        if (this.isChangeFinish)
+        {
+            if (this.SpriteList[this.currentIndex].color.a >= 1)
+            {
+                this.currentIndex--;
+                if (this.currentIndex < 0)
+                {
+                    this.currentIndex = 0;
+                    this.SpriteList[this.currentIndex].color.a = 1;
+                }
+                else
+                {
+                    this.isChangeFinish = false;
+                    iTween.ValueTo(this.gameObject, iTween.Hash(
+                        "name", "ColorChange",
+                        "from", this.SpriteList[this.currentIndex].color.a,
+                        "to", this.SpriteList[this.currentIndex].color.a + this.ChangeDeltaScale,
+                        "time", this.CountDownTime,
+                        "onupdate", "ColorChangeUpdate",
+                        "onupdateparams", this.updateValue,
+                        "oncomplete", "ColorChangeComplete"
+                        ));
+                }
+            }
+            else
+            {
+                this.isChangeFinish = false;
+                iTween.ValueTo(this.gameObject, iTween.Hash(
+                    "name", "ColorChange",
+                    "from", this.SpriteList[this.currentIndex].color.a,
+                    "to", this.SpriteList[this.currentIndex].color.a + this.ChangeDeltaScale,
+                    "time", this.CountDownTime,
+                    "onupdate", "ColorChangeUpdate",
+                    "onupdateparams", this.updateValue,
+                    "oncomplete", "ColorChangeComplete"
+                    ));
+            }
+        }
     }
 
     // Update is called once per frame
@@ -21,29 +142,29 @@ public class PictureColorController : MonoBehaviour
         //Alpha減少
         if (Input.GetKeyDown(KeyCode.KeypadMinus))
         {
-            //減少Alpha值
-            this.SpriteList[this.currentIndex].SetColor(new Color(1, 1, 1, this.SpriteList[this.currentIndex].color.a - this.ChangeScale));
-            if (this.SpriteList[this.currentIndex].color.a <= 0)
-            {
-                this.currentIndex++;
-                if (this.currentIndex >= this.SpriteList.Count)
-                    this.currentIndex = this.SpriteList.Count - 1;
-            }
+            DecPictureAlpha();
         }
 
         //Alpha增加
         if (Input.GetKeyDown(KeyCode.KeypadPlus))
         {
-            //增加Alpha值
-            this.SpriteList[this.currentIndex].SetColor(new Color(1, 1, 1, this.SpriteList[this.currentIndex].color.a + this.ChangeScale));
-            if (this.SpriteList[this.currentIndex].color.a >= 1)
-            {
-                this.currentIndex--;
-                if (this.currentIndex < 0)
-                    this.currentIndex = 0;
-            }
+            AddPictureAlpha();
         }
 
-        print("Current Picture Name：" + this.SpriteList[this.currentIndex].name + " ,Current Alpha = " + this.SpriteList[this.currentIndex].color.a);
+        if (!this.isStartRecove)
+        {
+            this.addValue -= Time.deltaTime;
+            if (this.addValue <= 0)
+            {
+                this.addValue = this.UndetectTime;
+                this.isStartRecove = true;
+            }
+        }
+        else
+        {
+            this.AddPictureAlpha();
+        }
+
+        //print("Current Picture Name：" + this.SpriteList[this.currentIndex].name + " ,Current Alpha = " + this.SpriteList[this.currentIndex].color.a);
     }
 }
