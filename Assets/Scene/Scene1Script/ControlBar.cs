6f479966b2ec87a4cfe208d6f2c19cf232534ae2
Ian-Huang <dk781020@hotmail.com>
2013-08-18 17:01:44 +0800

    G1updata

--- Assets/Scene/Scene1Script/ControlBar.cs
+++ Assets/Scene/Scene1Script/ControlBar.cs
@@ -12,34 +12,34 @@ public class ControlBar : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKey("q"))
+        if (Input.GetKey("q") || KinectDetectOutput.LeftHandGestureType == KinectDetectOutput.GestureType.Up)
         {
 
             RotateBridge01.transform.eulerAngles = new Vector3 (0, 0, -33);            
         }
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") || KinectDetectOutput.LeftHandGestureType == KinectDetectOutput.GestureType.Mid)
         {
             RotateBridge01.transform.eulerAngles = new Vector3(0, 0, 0);
         }
-        if (Input.GetKey("z"))
+        if (Input.GetKey("z") || KinectDetectOutput.LeftHandGestureType == KinectDetectOutput.GestureType.Dowm)
         {
 
             RotateBridge01.transform.eulerAngles = new Vector3(0, 0, 35);  
             
         }
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w") || KinectDetectOutput.RightHandGestureType == KinectDetectOutput.GestureType.Up)
         {
 
             RotateBridge02.transform.eulerAngles = new Vector3(0, 0, 35);  
             
         }
-        if (Input.GetKey("s"))
+        if (Input.GetKey("s") || KinectDetectOutput.RightHandGestureType == KinectDetectOutput.GestureType.Mid)
         {
 
             RotateBridge02.transform.eulerAngles = new Vector3(0, 0, 0);  
             
         }
-        if (Input.GetKey("x"))
+        if (Input.GetKey("x") || KinectDetectOutput.RightHandGestureType == KinectDetectOutput.GestureType.Dowm)
         {
 
             RotateBridge02.transform.eulerAngles = new Vector3(0, 0, -33);  
